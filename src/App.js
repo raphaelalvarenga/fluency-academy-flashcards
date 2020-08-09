dc8a0a8c2b9d21b099c746284a0fcfded4166500
Raphael Alvarenga <raphaelalvarenga2@gmail.com>
2020-08-09 11:12:19 -0300

    The code has been cleaned up of useless code.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
-import Flashcards from "./views/Flashcards";
-import { Container, GlobalStyle } from "./styled-components";
+import { GlobalStyle } from "./styled-components";
 import { BrowserRouter } from "react-router-dom";
 import Routes from "./components/Routes";
 
